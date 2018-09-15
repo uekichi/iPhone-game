157d8ccabcebf6065d9a3aec58682ea384147e3e
uekicihi <uekichi@qc.commufa.jp>
2018-09-16 00:06:34 +0900

    iPhone game の雛形

---
+++ sample/GameScene.swift
@@ -0,0 +1,43 @@
+//
+//  GameScene.swift
+//  sample
+//
+//  Created by ウエキチ on 2018/09/15.
+//  Copyright © 2018年 ウエキチ. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
